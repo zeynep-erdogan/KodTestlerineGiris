9f513e945c291561d707e2d8ec2c2b77a897dc7b
zeynep-erdogan <zeynep.aslan.erdogan@gmail.com>
2025-03-15 12:38:18 +0300

    Login ve Success sayfaları eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
+import { Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Login from './components/login';
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/main">
+            <h1>Main Page</h1>
+          </Route>
+          <Route exact path="/error">
+          <h1>Error Page</h1>
+          </Route>
+        </Switch>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+      .then((res) => {
+        const user = res.data.find(
+          (item) => item.password == form.password && item.email == form.email
+        );
+        if (user) {
+          setForm(initialForm);
+          history.push('/main');
+        } else {
+          history.push('/error');
+        }
+      });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+        />
+      </FormGroup>
+      <FormGroup check>
+        <Input
+          id="terms"
+          name="terms"
+          checked={form.terms}
+          type="checkbox"
+          onChange={handleChange}
+        />{' '}
+        <Label htmlFor="terms" check>
+          I agree to terms of service and privacy policy
+        </Label>
+      </FormGroup>
+      <FormGroup className="text-center p-4">
+        <Button color="primary">Sign In</Button>
+      </FormGroup>
+    </Form>
+  );
+}
